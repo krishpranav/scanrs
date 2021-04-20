a12e1728c0c509b474c32cca04e2d31d96df00b5
krishpranav <krisna.pranav@gmail.com>
2021-04-20 18:43:21 +0530

    done changes

--- src/main.rs
+++ src/main.rs
@@ -3,3 +3,13 @@
 extern crate getopts;
 
 use getopts::Options;
+use std::env;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
+use std::str::FromStr;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
+
+// max possible port
+
+const MAX: u16 = 65535
