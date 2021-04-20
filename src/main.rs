91ce4f9a8102f742e99d989d5cb4feb83aac499d
krishpranav <krisna.pranav@gmail.com>
2021-04-20 18:47:27 +0530

    added functions

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,28 @@ use std::thread;
 // max possible port
 
 const MAX: u16 = 65535
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optopt("j",
+    "",
+    "number of threads to use for concurrent scanning",
+    "THREADS");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => {
+            println!("error parsing options: {}", e.to_string())
+            return;
+        }
+    };
+
+    if matches.opt_present("h") || matches.free.is_empty() {
+        let brief = format!("Usage: {} -j THREADS IPADDR", program);
+        print!("{}", opts.usage(&brief));
+        return;
+    }
+}
