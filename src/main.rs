0c6dfc5a8fabc91908e79d43e77b571c38afe4fb
krishpranav <krisna.pranav@gmail.com>
2021-04-20 18:50:35 +0530

    added scan function

--- src/main.rs
+++ src/main.rs
@@ -68,3 +68,23 @@ fn main() {
     }
 }
 
+func scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+
+    loop {
+        match TcpStream::connect((addr, port)) {
+            ok(-) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+
+        port += num_threads;
+    }
+}
